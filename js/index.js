28faa9ced8e6c74b393a62904640824607d5c50f
s-i-fedorov <s.i.fedorov.87@gmail.com>
2024-02-17 11:18:39 +0200

    1

---
+++ js/index.js
@@ -0,0 +1,18 @@
+const a = 5;
+const b = 8;
+function sum(a, b) {
+  const result = a + b;
+  console.log(result);
+}
+function sub(a, b) {
+  const result = a - b;
+  console.log(result);
+}
+function mult(a, b) {
+  const result = a * b;
+  console.log(result);
+}
+
+sum(a, b);
+sub(a, b);
+mult(a, b);
